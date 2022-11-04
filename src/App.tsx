b74240d01334617b5821672f36696227b86d88e7
Henrique Barros <henrique1_barros@hotmail.com>
2022-11-03 22:05:31 -0300

    fix: routes import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Form from "./components/Form";
 import Modal from "./components/Modal";
 import Button from "./components/Button";
 import GlobalStyle from "./style/global";
-import Routes from "./routes";
+import Routes from "./routes/index";
 
 
 function App() {
