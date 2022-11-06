6d935e23cd59321c939ba6702677af9b324735b5
Renata <re_juraski@hotmail.com>
2022-11-06 19:14:09 -0300

    feat: product page created

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,22 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../pages/Home";
+import Product from "../pages/Product";
 import ProfileView from "../pages/ProfileView";
 
 export default function Routes() {
-    return (
-      <Router>
-          <Switch>
-            <Route exact path="/home">
-              <Home />
-            </Route>
-            <Route path="/profile">
-              <ProfileView />
-            </Route>
-
-          </Switch>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route path="/profile">
+          <ProfileView />
+        </Route>
+        <Route path="/product">
+          <Product />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
