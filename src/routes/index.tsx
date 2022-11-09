6858a7d38db073f8b08bea0a0297a428f58111c3
kennedybm <kennedy_muniz@hotmail.com>
2022-11-09 09:53:04 -0300

    Fix: fixing routes sintax

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,16 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import ProfileView from "../pages/ProfileView";
 
 export default function Routes() {
-    return (
-      <Router>
-          <Switch>
-            <Route exact path="/home">
-              <Home />
-            </Route>
-            <Route path="/profile">
-              <ProfileView />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-          </Switch>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/profile" component={ProfileView} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
+  );
+}
