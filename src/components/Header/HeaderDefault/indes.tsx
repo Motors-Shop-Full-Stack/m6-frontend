c5777ec8749cbdda760331eb00769afcbad766d4
“Henrique <henrique1_barros@hotmail.com>
2022-11-10 22:04:47 -0300

    fix: header structure and provider

---
+++ src/components/Header/HeaderDefault/indes.tsx
@@ -0,0 +1,54 @@
+import Logo from "../../../assets/logo.png"
+import { Container, Item, UserBox } from "./styles";
+import { BiMenu, BiX } from "react-icons/bi";
+import Button from "../../Button";
+import { useHeader } from "../../../providers/HeaderProvider";
+
+const HeaderDefault = () => {
+  const { isOpen, setIsOpen } = useHeader();
+    return ( 
+    <Container isOpen={isOpen}>
+        <div className="container-img">
+          <img src={Logo} alt="logo" />
+          {!isOpen ? (
+            <BiMenu className="icon" onClick={() => setIsOpen(true)} />
+          ) : (
+            <BiX className="icon" onClick={() => setIsOpen(false)} />
+          )}
+        </div>
+        <div className="container-nav">
+          <nav>
+            <Item isOpen={isOpen}>
+              <a>Carros</a>
+            </Item>
+            <Item isOpen={isOpen}>
+              <a>Motos</a>
+            </Item>
+            <Item isOpen={isOpen}>
+              <a>Leilão</a>
+            </Item>
+          </nav>
+          <UserBox isOpen={isOpen}>
+              <div className="user_box-default">
+                <Item isOpen={isOpen}>
+                  <a>Fazer Login</a>
+                </Item>
+                <div className="user_box-button">
+                  <Button
+                    borderC="--grey4"
+                    backgroundC="--whiteFixed"
+                    fontC="--grey0"
+                    width="100px"
+                    height="50px"
+                  >
+                    Cadastrar
+                  </Button>
+                </div>
+              </div>
+          </UserBox>
+        </div>
+      </Container>
+    );
+  };
+ 
+export default HeaderDefault;
---
+++ src/components/Header/HeaderLogged/index.tsx
@@ -0,0 +1,63 @@
+import Logo from "../../../assets/logo.png"
+import { Container, Item, UserBox, UserIcon } from "./styles";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { BiMenu, BiX } from "react-icons/bi";
+import { useHeader } from "../../../providers/HeaderProvider";
+
+const HeaderLogged = ({data}: any) => {
+  const { isOpen, setIsOpen, open, getInitials, formatName, handleClick, handleClose, anchorEl } = useHeader();
+    return ( 
+        <Container isOpen={isOpen}>
+            <div className="container-img">
+              <img src={Logo} alt="logo" />
+              {!isOpen ? (
+                <BiMenu className="icon" onClick={() => setIsOpen(true)} />
+              ) : (
+                <BiX className="icon" onClick={() => setIsOpen(false)} />
+              )}
+            </div>
+            <div className="container-nav">
+              <nav>
+                <Item isOpen={isOpen}>
+                  <a>Carros</a>
+                </Item>
+                <Item isOpen={isOpen}>
+                  <a>Motos</a>
+                </Item>
+                <Item isOpen={isOpen}>
+                  <a>Leilão</a>
+                </Item>
+              </nav>
+              <UserBox isOpen={isOpen}>
+                  <>
+                    <div
+                      id="basic-button"
+                      aria-controls={open ? "basic-menu" : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={open ? "true" : undefined}
+                      onClick={handleClick}
+                      className="user_box-div"
+                    >
+                      <UserIcon>{data && getInitials(data.name)}</UserIcon>
+                      <h5>{data && formatName(data.name)}</h5>
+                    </div>
+                    <Menu
+                      id="basic-menu"
+                      anchorEl={anchorEl}
+                      open={open}
+                      onClose={handleClose}
+                    >
+                      <MenuItem>Editar perfil</MenuItem>
+                      <MenuItem>Editar endereço</MenuItem>
+                      <MenuItem>Minhas compras</MenuItem>
+                      <MenuItem>Sair</MenuItem>
+                    </Menu>
+                  </>
+              </UserBox>
+            </div>
+          </Container>
+        );
+}
+ 
+export default HeaderLogged;
